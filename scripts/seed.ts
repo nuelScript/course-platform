1297556ff5f0cf05421ab827d701313300925b7e
Parseval <igwee3333@gmail.com>
2024-02-05 08:54:55 +0100

    chore: changed chapters to modules

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -5,15 +5,7 @@ const database = new PrismaClient();
 async function main() {
   try {
     await database.category.createMany({
-      data: [
-        { name: "Computer Science" },
-        { name: "Music" },
-        { name: "Fitness" },
-        { name: "Photography" },
-        { name: "Accounting" },
-        { name: "Engineering" },
-        { name: "Filming" },
-      ]
+      data: [{ name: "Teachers" }, { name: "Children" }],
     });
 
     console.log("Success");
